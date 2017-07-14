95dc332329c21eb8e5ae2a5adad952524647fa88
Alikeju Adejo <alikejuadejo@yahoo.com>
2017-07-14 13:49:56 -0400

    Updated the PostImageCell.swfit file

---
+++ Makestagram/PostImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  PostImageCell.swift
+//  Makestagram
+//
+//  Created by Alikeju Adejo on 7/14/17.
+//  Copyright © 2017 Alikeju Adejo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostImageCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
