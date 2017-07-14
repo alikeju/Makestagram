f6eb64d7596fa5f79e64bb824b564d8f00da003a
Alikeju Adejo <alikejuadejo@yahoo.com>
2017-07-14 13:47:30 -0400

    Updated the FindFriendsViewController.swift class

---
+++ Makestagram/FindFriendsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FindFriendsViewController.swift
+//  Makestagram
+//
+//  Created by Alikeju Adejo on 7/13/17.
+//  Copyright © 2017 Alikeju Adejo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FindFriendsViewController: UIViewController{
+    
+}
